4bed761538494687a44575134510857436501f2c
Anton Barysenka <anton_barysenka@epam.com>
2024-03-13 21:08:54 +0300

    dev: preloader

--- js/app.js
+++ js/app.js
@@ -7,4 +7,19 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
   wrapper: '.js-wrapper',
   content: '.js-content'
-})
+});
+
+ // preloader
+
+document.body.setAttribute("class", "noscroll");
+
+document.getElementById("overlay").style.display = "block";
+document.getElementById("spinner").style.display = "block";
+
+
+window.onload = function() {
+  document.getElementById("spinner").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+
+  document.body.className = document.body.className.replace(/\bnoscroll\b/,'');
+}
